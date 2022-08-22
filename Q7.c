24340ac5f09c8b76216371cc0fa8daf3fc375d0a
Ashish Kumar Sahoo <111702590+PrototypeMK14@users.noreply.github.com>
2022-08-22 18:58:07 +0530

    Add files via upload

--- Q7.c
+++ Q7.c
@@ -2,22 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-    char number[100];
-    int count=0;
+int main()
+{
+    int n;
+    printf("Enter the no. digits of a number : ");
+    scanf("%d", &n);
+
+    char number[n];
+    int count = 0;
     printf("Enter a number : ");
-    gets(number);
-    int j=strlen(number)-1;
-    for(int i=0;i<strlen(number);i++) {
-        if(number[i]==number[j]) {
+    scanf("%s", &number);
+    int j = strlen(number) - 1;
+    for (int i = 0; i < strlen(number); i++)
+    {
+        if (number[i] == number[j])
+        {
             count++;
         }
         j--;
     }
 
-    if(count==strlen(number)) 
-        printf("%s is a pallindrome number!!",number);
-    else 
-        printf("%s is not a pallindrome number!!",number);
+    if (count == strlen(number))
+        printf("%s is a pallindrome number!!", number);
+    else
+        printf("%s is not a pallindrome number!!", number);
     return 0;
 }
