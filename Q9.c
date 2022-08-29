71c3a7641e49eb5067bacabcd83dd599aadfb1c9
Ashish Kumar Sahoo <111702590+PrototypeMK14@users.noreply.github.com>
2022-08-29 17:54:51 +0530

    Add files via upload

--- Q9.c
+++ Q9.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main() {
-    int num,x,sum=0,n;
-    printf("Enter the value of n : ");
-    scanf("%d",&n);
+int main()
+{
+    printf("Given polynomial is of format P(x) = AnXn+An-1*Xn-1+An-2*Xn-2+.....+A0X0\n");
+    float num, x, sum = 0, n;
+    int i;
+    printf("Enter the degree of polynomial : ");
+    scanf("%f", &n);
     printf("Enter the value of x : ");
-    scanf("%d",&x);
-    for(int i=n;i>=0;i--) {
-        scanf("%d",&num);
-        sum+=num*pow(x,i);
+    scanf("%f", &x);
+    for (i = n; i >= 0; i--)
+    {
+        printf("Enter the co-effieceint of x%d : ", i);
+        scanf("%f", &num);
+        sum += num * pow(x, i);
     }
 
-    printf("The value of polynomail P(%d) is = %d",x,sum);
+    printf("The value of polynomail P(%.2f) is = %.2f", x, sum);
     return 0;
 }
